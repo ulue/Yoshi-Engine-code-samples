7b40cbb15a8f7f6e544f3956d5f7722231f1393c
YoshiCrafter29 <yoshicrafter.mkpc@gmail.com>
2022-02-06 13:31:56 +0100

    Create Add a FlxSprite.lua

---
+++ Add a FlxSprite.lua
@@ -0,0 +1,20 @@
+-- Check the Lua documentation for more info
+-- https://github.com/YoshiCrafter29/YoshiEngine/blob/main/documentation/doc/lua.md
+
+function create()
+    -- If you wanna load a Sparrow Atlas
+    createClass("YourSprite", "FlxSprite", {0, 0}) -- Replace 0, 0 by the sprite's position
+
+    call("Paths.getSparrowAtlas", "SparrowAtlas", {"path/to/sparrow"}) -- Replace "path/to/sparrow" by your sparrow path. For example, "stage1/bgPeople" for "images/stage1/bgPeople.png" and "images/stage1/bgPeople.xml"
+    set("YourSprite.frames", v("SparrowAtlas")) -- Sets the frames
+    call("YourSprite.animation.addByPrefix", nil, {"Animation Name", "Animation XML Name", 24, true}) -- Check https://api.haxeflixel.com/flixel/animation/FlxAnimationController.html#addByPrefix
+    call("YourSprite.animation.play", nil, {"Animation Name"})
+    call("PlayState.add", {v("YourSprite")}) -- Adds the sprite to the PlayState
+
+    -- If you wanna load a simple bitmap
+    createClass("YourSprite", "FlxSprite", {0, 0}) -- Replace 0, 0 by the sprite's position
+
+    call("Paths.image", "Bitmap", {"path/to/bitmap"}) -- Replace "path/to/sparrow" by your sparrow path. For example, "stage1/bgPeople" for "images/stage1/bgPeople.png"
+    call("YourSprite.loadGraphic", nil, {v("Bitmap")}) -- Loads the bitmap
+    call("PlayState.add", {v("YourSprite")}) -- Adds the sprite to the PlayState
+end
