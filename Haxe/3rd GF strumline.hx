7d1b7724bb0c1fc5c3fbac52fea978be46ab94b4
YoshiCrafter29 <81191621+YoshiCrafter29@users.noreply.github.com>
2022-06-23 17:49:24 +0200

    PlayState_.SONG.keyNumber fix

--- Haxe/3rd GF strumline.hx
+++ Haxe/3rd GF strumline.hx
@@ -9,8 +9,8 @@ function onGenerateStaticArrows() {
     }
 
     PlayState.generateStaticArrows();
-    for(i in 0...PlayState.song.keyNumber) {
-        var strum = PlayState.cpuStrums.members[i + PlayState.song.keyNumber];
+    for(i in 0...PlayState_.SONG.keyNumber) {
+        var strum = PlayState.cpuStrums.members[i + PlayState_.SONG.keyNumber];
         strum.x = (FlxG.width / 2) + ((i - 2) * (Note.swagWidth * 0.8));
         strums.push(strum);
     }
@@ -20,8 +20,8 @@ function updatePost(elapsed:Float) {
     PlayState.notes.forEach(function(n) {
         if (n.noteType == 1) {
             // gf note
-            var strum = PlayState.cpuStrums.members[n.noteData % PlayState.song.keyNumber];
-            var nStrum = strums[n.noteData % PlayState.song.keyNumber];
+            var strum = PlayState.cpuStrums.members[n.noteData % PlayState_.SONG.keyNumber];
+            var nStrum = strums[n.noteData % PlayState_.SONG.keyNumber];
             n.x = n.x - strum.x + nStrum.x;
             n.y = n.y - strum.y + nStrum.y;
         }
@@ -30,7 +30,7 @@ function updatePost(elapsed:Float) {
 function onDadHit(note:Note) {
     if (EngineSettings.glowCPUStrums = !(note.noteType == 1)) {
     } else {
-        var nStrum = strums[note.noteData % PlayState.song.keyNumber];
+        var nStrum = strums[note.noteData % PlayState_.SONG.keyNumber];
         nStrum.cpuRemainingGlowTime = note.stepLength * 1.5 / 1000;
         nStrum.animation.play("confirm", true);
         nStrum.centerOffsets();
